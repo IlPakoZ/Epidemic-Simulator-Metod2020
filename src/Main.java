b2cfca5ada4e5ce054f2af33f5ed43744ca03fdc
Pasquale Lobascio <pasquale.lobascioo@gmail.com>
2020-05-19 16:25:37 +0200

    Revert "Revert "Revert "Ho scritto una cosa stupida"""
    This reverts commit 0f884754ef482309bc76ee4800a040d963290c08.

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,5 @@ public class Main {
     public static void main(String[] args) {
         Core x = new Core();
         x.newSimulation(new CommandLineMenu());
-        System.out.println("Ho il coronavirus");
     }
 }
