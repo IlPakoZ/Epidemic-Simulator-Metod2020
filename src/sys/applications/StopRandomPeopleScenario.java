37dd9b45121c2552d8a72c1bbbb230bd6c377043
MrGerboh <gervasio.1883259@studenti.uniroma1.it>
2020-06-21 14:40:51 +0200

    Creati gli scenari già pronti

---
+++ src/sys/applications/StopRandomPeopleScenario.java
@@ -0,0 +1,58 @@
+package sys.applications;
+
+import assets.MovementStatus;
+import assets.Person;
+import sys.Simulation;
+import sys.Core.*;
+import sys.State;
+import sys.models.Scenario;
+import java.util.Random;
+
+public class StopRandomPeopleScenario extends Scenario{
+
+    private Simulation currentSimulation;
+    private State currentState;
+    private int peopleToStop;
+    private static final int ID = 3;
+
+    public StopRandomPeopleScenario(Simulation currentSimulation, int people) {
+        super(currentSimulation);
+        currentState = currentSimulation.getCurrentState();
+        peopleToStop = people;
+    }
+
+    @Override
+    public void oneTimeAction() {
+
+    }
+
+    @Override
+    public void dailyAction() {
+        Random r = new Random();
+        for (int i = 0; i < peopleToStop; i++) {
+            Person x = currentState.startingPopulation[r.nextInt(currentState.blueBlack)];
+            while (currentState.swabs.contains(x)) x = currentState.startingPopulation[r.nextInt(currentState.blueBlack)];
+            x.movement = MovementStatus.STATIONARY;
+        }
+    }
+
+    @Override
+    public void frameAction() {
+
+    }
+
+    @Override
+    public int getID() {
+        return ID;
+    }
+
+    @Override
+    public String getInfos() {
+        return "In questo scenario, ogni giorno vengono fermate un numero preso in input di persone.\n";
+    }
+
+    @Override
+    public String getName() {
+        return "StopRandomPeopleScenario\n";
+    }
+}
